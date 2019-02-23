1b86322f280b1775a7cffcdac8bd6c8f44fa355b
Ashley Armstrong <D00213195@student.dkit.ie>
2019-02-23 13:24:21 +0000

    Models
    If implemented this update will add select, insert, delete and updates
    to the car_m_model and the car_brand_model.

--- lib/model/car_brand_model.php
+++ lib/model/car_brand_model.php
@@ -1,22 +1,55 @@
 <?php
-class car_brand_model {
-private $brand_name;
-private $brand_status;
-private $establishment;
-private $parent_company;
+    class car_brand_model {
+    private $brand_name;
+    private $brand_status;
+    private $establishment;
+    private $parent_company;
 
-public function __construct($row){
-    $this->brand_name = $row['brand_name'];
-    $this->brand_status = $row['brand_status'];
-    $this->establishment = $row['establishment'];
-    $this->parent_company = $row['parent_company'];
-}
-public static function returnCar($db){
-    $sc = 'select * from car_brand;';
-    $s = $db->prepare($sc);
-    $s->execute();  
-    $array = $s->fetchALL();
-    return $array;
-}
-}
-?>
+    public function __construct($row){
+        $this->brand_id = $row['brand_id'],
+        $this->brand_name = $row['brand_name'];
+        $this->brand_status = $row['brand_status'];
+        $this->establishment = $row['establishment'];
+        $this->parent_company = $row['parent_company'];
+    }
+
+    public static function return_Car($db){
+        $statement_Select = 'SELECT * FROM car_brand;';
+        $sS = $db->prepare($statement_Select);
+        $sS->execute();  
+        $array = $sS->fetchALL();
+        return $array;
+    }
+
+    public static function add_Brand($db, $args){
+        $statement_Insert = 'INSERT INTO car_brand (brand_name, brand_status, brand_establishment, parent_company, brand_net_worth) VALUES (:brand_name, :brand_status, :brand_establishment, :parent_company, :brand_net_worth);';
+        $sI = $db->prepare($statement_Insert);
+        $sI->execute([
+            'brand_name' =>$args['brand_name'],
+            'brand_status'=>$args['brand_status'],
+            'brand_establishment'=>$args['brand_establishment'],
+            'parent_company'=>$args['parent_company'],
+            'brand_net_worth'=>$args['brand_net_worth']
+        ]);  
+    }
+
+    public static function delete_Brand($db, $args){
+        $statement_Delete = "DELETE FROM car_brand WHERE brand_id=:brand_id";
+        $sD = $db->prepare($statement_Delete);
+        $sD->bindValue(":brand_id", $args['brand_id']);
+        $sD->execute();
+    }
+
+    public static function update_Brand($db, $args){
+        $statement_Update = 'UPDATE car_brand SET (brand_name = :brand_name, brand_status = :brand_status, brand_establishment = :brand_establishment, parent_company = :parent_company, brand_net_worth = :brand_net_worth) WHERE brand_id = :brand_id;';
+        $sU = $db->prepare($statement_Update);
+        $sU->bindValue(":brand_id", $args['brand_id']);
+        $sU->execute([
+            'brand_name' =>$args['brand_name'],
+            'brand_status'=>$args['brand_status'],
+            'brand_establishment'=>$args['brand_establishment'],
+            'parent_company'=>$args['parent_company'],
+            'brand_net_worth'=>$args['brand_net_worth']
+        ]); 
+    }
+}?>
--- lib/model/car_m_model.php
+++ lib/model/car_m_model.php
@@ -1,28 +1,68 @@
 <?php
-class car_m_model {
-private $_id;
-private $brand_id;
-private $model_name;
-private $year_introduced;
-private $year_discontinued;
-private $body_type;
-private $hybrid;
-private $fuel_type;
+    class car_m_model {
+    private $_id;
+    private $brand_id;
+    private $model_name;
+    private $year_introduced;
+    private $year_discontinued;
+    private $body_type;
+    private $hybrid;
+    private $fuel_type;
 
-public function __construct($row){
-    $this->id = $row['model_id'];
-    $this->brand_id = $row['brand_id'];
-    $this->model_name = $row['model_name'];
-    $this->year_introduced = $row['year_introduced'];
-    $this->year_discontinued = $row['year_discontinued'];
-    $this->body_type = $row['body_type'];
-    $this->hybrid = $row['hybrid'];
-    $this->fuel_type = $row['fuel_type'];
-}
-public static createNew($args){
+    public function __construct($row){
+        $this->model_id = $row['model_id'];
+        $this->brand_id = $row['brand_id'];
+        $this->model_name = $row['model_name'];
+        $this->year_introduced = $row['year_introduced'];
+        $this->year_discontinued = $row['year_discontinued'];
+        $this->body_type = $row['body_type'];
+        $this->hybrid = $row['hybrid'];
+        $this->fuel_type = $row['fuel_type'];
+    }
+    
+    public static function return brand_Model($db, $args){
+        $statement_Select = 'SELECT * FROM car_model WHERE brand_id = :brand_id;';
+        $sS = $db->prepare($statement_Select);
+        $sS->bindValue(":brand_id", $args['brand_id']);
+        $sS->execute();  
+        $array = $sS->fetchALL();
+        return $array;
+    }
 
-    $database->prepare('INSERT INTO car_model (brand_id, model_name, year_introduced, year_discontinued, body_type, hybrid, fuel_type) values()');
-    $database->excecute([]);
-}
+    public static function add_Model($db,$args){
+        $statement_Insert = 'INSERT INTO car_model (brand_id, model_name, year_introduced, year_discontinued, body_type, hybrid, fuel_type) VALUES (:brand_id, :model_name, :year_introduced, :year_discontinued, :body_type, :hybrid, :fuel_type);';
+        $sI = $db->prepare($statement_Insert);
+        $sI->execute([
+            'brand_id' =>$args['brand_id'],
+            'model_name'=>$args['model_name'],
+            'year_introduced'=>$args['year_introduced'],
+            'year_discontinued'=>$args['year_discontinued'],
+            'body_type'=>$args['body_type'],
+            'hybrid'=>$args['hybrid'],
+            'fuel_type'=>$args['fuel_type']
+        ]);  
+    }
+
+    public static function delete_Model($db, $args){
+        $statement_Delete = "DELETE FROM car_model WHERE model_id=:model_id";
+        $sD = $db->prepare($statement_Delete);
+        $sD->bindValue(":model_id", $args['model_id']);
+        $sD->execute();
+    }
+
+    public static function update_Model($db, $args){
+        $statement_Update = 'UPDATE car_model SET (brand_id = :brand_id, model_name = :model_name, year_introduced = :year_introduced, year_discontinued = :year_discontinued, body_type = :body_type, hybrid = :hybrid, fuel_type = :fuel_type) WHERE model_id = :model_id;';
+        $sU = $db->prepare($statement_Update);
+        $sU->bindValue(":model_id", $args['model_id']);
+        $sU->execute([
+            'brand_id' =>$args['brand_id'],
+            'model_name'=>$args['model_name'],
+            'year_introduced'=>$args['year_introduced'],
+            'year_discontinued'=>$args['year_discontinued'],
+            'body_type'=>$args['body_type'],
+            'hybrid'=>$args['hybrid'],
+            'fuel_type'=>$args['fuel_type']
+        ]); 
+    }
 }
 ?>
