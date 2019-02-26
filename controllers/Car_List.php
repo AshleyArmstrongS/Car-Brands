bcf4c2ac218b2a38dbdc9b454c5b3c5b65ca05a4
Ashley Armstrong <D00213195@student.dkit.ie>
2019-02-26 18:07:28 +0000

    fix convention
    fix a variable call to follow convention

--- controllers/Car_List.php
+++ controllers/Car_List.php
@@ -5,7 +5,8 @@
     require('./model/car_brand_model.php'); 
     require('./model/car_m_model.php'); 
     $car_brand = car_brand_model::return_Car($db);
-    $brand_model = car_m_model::return_Brand_Model($db, $req->query('brand_id'));
+    $brand_id = $req->query('brand_id');
+    $brand_model = car_m_model::return_Brand_Model($db, $brand_id);
     $res->render('main','car_list',[
     'return_Car' => $car_brand,
     'return_Brand_Model' => $brand_model
